dfe36c63a8664b3551a4fd2d6b649ea350a4e876
iamSamyakVansh <133241891+iamSamyakVansh@users.noreply.github.com>
2024-05-13 21:25:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const skillItems = document.querySelectorAll('.skill.technical');
+
+// skillItems.forEach(item => {
+//   item.addEventListener('click', () => {
+//     const content = item.dataset.content;
+//     alert(content);
+//   });
+// });
